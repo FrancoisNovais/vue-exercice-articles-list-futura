c4ab9cd38ad6e8598b8e35cca8954afb84371aad
francois6381 <francois.novais@gmail.com>
2024-07-26 15:30:44 +0200

    Footer component elements

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
 import Header from './components/Header.vue'
 import Frontpage from './components/Frontpage.vue'
 import Article from './components/Article.vue'
+import Footer from './components/Footer.vue'
 </script>
 
 <template>
   <Header />
   <Frontpage />
   <Article />
+  <Footer />
 </template>
 
 <style scoped></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,17 @@
+<script setup></script>
+
+<template>
+  <footer>
+    <p>
+      Réalisé par <a href="https://github.com/FrancoisNovais">François</a> avec
+      <a href="https://vuejs.org/">Vue.js</a>
+    </p>
+  </footer>
+</template>
+
+<style scoped>
+footer {
+  border: 2px solid orange;
+  margin: 20px;
+}
+</style>
